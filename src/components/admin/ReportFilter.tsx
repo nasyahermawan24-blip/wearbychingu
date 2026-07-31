cb10b2fd5060b92a18535b70f420c6286369d5cd
nasyahermawan <nasyasaja2@gmail.com>
2026-08-01 00:30:10 +0700

    fix report filter type

--- src/components/admin/ReportFilter.tsx
+++ src/components/admin/ReportFilter.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { ReportFilter } from "@/types/filter";
+import { ReportFilter as ReportFilterType } from "@/types/filter";
 
 interface Props {
-  value: ReportFilter;
-  onChange: (value: ReportFilter) => void;
+  value: ReportFilterType;
+  onChange: (value: ReportFilterType) => void;
 }
 
 const filters: {
